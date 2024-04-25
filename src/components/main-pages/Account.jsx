c9a99955c8aca1c8ce43f03ea8c0887bb6ffdfcc
Anushka Lakmal <anushkalakmal0607@gmail.com>
2024-04-25 22:16:35 +0530

    update account component

--- src/components/main-pages/Account.jsx
+++ src/components/main-pages/Account.jsx
@@ -1,33 +1,48 @@
 import React from 'react';
 import Footer from './Footer';
-//react icons
-import { RiAccountCircleLine } from "react-icons/ri";
+import holi1 from "../../images/Holi 1.png";
+import holi2 from "../../images/Holi 2.png";
+import { MdAccountCircle } from "react-icons/md";
+
 
 export default function Account() {
 
-  const userName ="Ishara Wanninayaka";
-  const contactNo ="071 53 89 697" ;
-  const email ="isharawanninayaka@gmail.com";
+  const userName = "Ishara Wanninayaka";
+  const contactNo = "071 53 89 697";
+  const email = "isharawanninayaka@gmail.com";
 
 
   return (
-      <div className="wrapper">
-        <div className='account-main'>
-          <div>
-            <p className='acount-text'>your Account</p>
-          </div>
+    <div className="wrapper">
+      <div className='w-full bg-[rgb(25,31,92)] h-[69px] flex flex-row'>
+        <div className='float-left basis-1/5'>
+          <img className='mt-4' src={holi1} alt='' />
+        </div>
 
-          <div className='account-icon'>
-            <RiAccountCircleLine size={150} color='rgb(25,31,92)'/>
-          </div>
+        <div className='basis-3/5'>
+          <p className='mt-[7.59px] text-center text-white text-[20px] font-bold font-montserat'>CONTACT US</p>
+          <p className='mt-[1px] text-center text-white text-[14px] font-light font-montserat'>GET IN TOUCH EASILY</p>
+        </div>
 
-          <div className='account-details'>
-            <div className='text-acc-details'><p className="name">Name</p><span className="name">{userName}</span></div>
-            <div className='text-acc-details'><p className="phone">Phone</p><span className="phone">{contactNo}</span></div>
-            <div className='text-acc-details'><p className="email">Email</p><span className="email">{email}</span></div>
-          </div>
-          <div className='bottom-headder'><Footer/></div>
+        <div className=' basis-1/5'>
+          <img className='float-right' src={holi2} alt='' />
         </div>
       </div>
+
+      <div>
+        <div>
+          <MdAccountCircle size={45} />
+        </div>
+        <div>
+
+        </div>
+      </div>
+
+
+      <div className='bottom-headder'><Footer/></div>
+
+
+
+    </div>
   )
 }
