44a9f70a3083390fec48a2037ac276f312f879a9
Anushka Lakmal <anushkalakmal0607@gmail.com>
2024-04-27 22:30:10 +0530

    Account jsx updated

--- src/components/main-pages/Account.jsx
+++ src/components/main-pages/Account.jsx
@@ -7,7 +7,7 @@ import { MdAccountCircle } from "react-icons/md";
 
 export default function Account() {
 
-  const userName = "Ishara Wanninayaka";
+  const userName = "Devinda wanasingha";
   const contactNo = "071 53 89 697";
   const email = "isharawanninayaka@gmail.com";
 
@@ -20,8 +20,8 @@ export default function Account() {
         </div>
 
         <div className='basis-3/5'>
-          <p className='mt-[7.59px] text-center text-white text-[20px] font-bold font-montserat'>CONTACT US</p>
-          <p className='mt-[1px] text-center text-white text-[14px] font-light font-montserat'>GET IN TOUCH EASILY</p>
+          <p className='mt-[7.59px] text-center text-white text-[20px] font-bold font-montserat'>YOUR ACCOUNT</p>
+          <p className='mt-[1px] text-center text-white text-[14px] font-light font-montserat'>EFFORTLESS MANAGEMENT</p>
         </div>
 
         <div className=' basis-1/5'>
@@ -29,19 +29,27 @@ export default function Account() {
         </div>
       </div>
 
-      <div>
-        <div>
-          <MdAccountCircle size={45} />
-        </div>
-        <div>
 
+
+
+      <div className='w-[266px] m-auto flex flex-raw pt-8'>
+        <div className='w-[73px] h-auto'>
+          <MdAccountCircle size={73} color='#191F5C'  />
+        </div>
+        <div className='pt-3 w-[153px] h-auto '>
+          <p className='text-[14px]'>{userName}</p>
+          <p className='text-[14px]'>{email}</p>
+          <p className=' text-[14px]'>{contactNo}</p>
         </div>
       </div>
-
+      <div className='w-[266px] bg-[rgb(25,31,92)] h-[41px] m-auto mt-8 flex flex-row rounded-full'>
+        <div className=' h-[24px] text-white font- Montserrat font-bold  text-[20px] pt-2 pl-[48px]'>
+          <p>YOUR COURSES</p>
+        </div>
+        
+      </div>
 
       <div className='bottom-headder'><Footer/></div>
-
-
 
     </div>
   )
