f9a229516997fc72f0bf79ed2efb81bace545497
IsharaWanninayaka <IsharaWanninayaka@users.noreply.github.com>
2024-04-27 23:56:38 +0530

    develop page-register

--- src/components/login-pages/Login.jsx
+++ src/components/login-pages/Login.jsx
@@ -12,6 +12,7 @@ import { RiLockPasswordFill } from "react-icons/ri";
 
 
 import "../../css/style.scss";
+import { Link } from "react-router-dom";
 function Login() {
   return (
     <div className="h-auto">
@@ -35,14 +36,17 @@ function Login() {
           </div>
         </div>
 
-        <form className="w-[210px] h-[295px] bg-[rgb(25,31,92)] mt-[39px] rounded-[28px] m-auto flex flex-col">
+        <form >
+            <div className="w-[210px] h-[295px] bg-[rgb(25,31,92)] mt-[39px] rounded-[28px] m-auto flex flex-col">
                 <div className="w-[192px] h-[44px] bg-white  m-2 rounded-[72px] flex flex-raw">
                     <div className="w-[93px] h-[38px] bg-[rgb(25,31,92)] rounded-[72px] m-[3px] basis-1/2 hover:cursor-pointer">
                       <p className="text-[14px] font-montserat mt-2 ml-5 font-black text-white">LOGIN</p>
                     </div>
-                    <div className="w-[93px] h-[38px] basis-1/2 hover:cursor-pointer">
-                      <p className="text-[rgb(25,31,92)] text-[14px] font-montserat mt-[11px] ml-2 font-black">RGISTER</p>
-                    </div>
+                    <Link to="/register">
+                        <div className="w-[93px] h-[38px] basis-1/2 hover:cursor-pointer">
+                          <p className="text-[rgb(25,31,92)] text-[14px] font-montserat mt-[11px] ml-[13px] font-black">RGISTER</p>
+                        </div>
+                    </Link>
                 </div>
 
                 <div className="w-[173px] h-[31px] flex flex-row ml-[18.5px] mt-2">
@@ -65,19 +69,20 @@ function Login() {
 
                 <div className="w-[173px] ml-[18.5px]">
                   <input type="checkbox"  name="rememberPassword" value="" className="w-[7px] h-[7px]"/>
-                  <label for="remember " className="font-montserat text-[8px] text-white ml-1">Remember Password </label>
+                  <label for="remember " value="True" className="font-montserat text-[8px] text-white ml-1">Remember Password </label>
                   <label for="" className="font-montserat text-[8px] text-white ml-[5px] ">Foget Password </label>
                 </div>
 
                 <div className="w-[186px] h-auto ml-[11px] mt-[26px]"><img src={jenzeict} alt=""/></div>
-        </form>
 
-        <div className="m-auto w-[102px] h-[36px] ">
-            <button type="submit" name="submit" className=" w-[102px] h-[36px] bg-[rgb(25,31,92)] mt-2 rounded-full flex ">
-                <img src={loginlogo} alt="" className="w-8 h-8 mt-[2px] ml-[3px]"/>
-                <p className="text-white font-montserat text-[15px] font-bold mt-2 ml-1">LOGIN</p>
-            </button>
-        </div>
+            </div>
+            <div className="m-auto w-[102px] h-[36px] ">
+              <button type="submit" name="submit" className=" w-[102px] h-[36px] bg-[rgb(25,31,92)] mt-2 rounded-full flex ">
+                  <img src={loginlogo} alt="" className="w-8 h-8 mt-[2px] ml-[3px]"/>
+                  <p className="text-white font-montserat text-[15px] font-bold mt-2 ml-1">LOGIN</p>
+              </button>
+            </div>
+        </form>
         
         <div className="w-full h-[110px] bg-[rgb(25,31,92)] m-auto mt-[39px]">
           <p className="font-bold text-white text-[14px] pt-2 text-center font-montserat">Meet Our Web Development Team</p>
--- src/components/login-pages/Register.jsx
+++ src/components/login-pages/Register.jsx
@@ -1,73 +1,220 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import email_icon from '../../images/Email.png';
-import password_icon from '../../images/Password.png';
-import register_icon from '../../images/Register_icon.png';
-import mobile_icon from '../../images/smartphone.png';
-import name_icon from '../../images/id-card.png';
+import React from "react";
+import holi1 from "../../images/Holi 1.png";
+import holi2 from "../../images/Holi 2.png";
+import jenzeict2 from '../../images/genZictclass LOGO.png';
+import loginlogo from '../../images/login.png';
+import memberHoli from '../../images/Member_Holi 1.png';
+import memberHoli2 from '../../images/Member Holi 2 1.png';
+import { IoMdMail } from "react-icons/io";
+import { FaCircleUser } from "react-icons/fa6";
+import { FaMobile } from "react-icons/fa6";
+import { RiLockPasswordFill } from "react-icons/ri";
+//import { Link } from 'react-router-dom';
 
-import '../../css/style.scss';
 
-function Register() {
+import "../../css/style.scss";
+import { Link } from "react-router-dom";
+function Login() {
   return (
-    <div className='reg_auth'>
-        <div className='form-box'>
-            <div className='buttonbox'>
-                <div id='btn'></div>
-                <button type='button' className='toggle-btn'><Link to="/login">LOGIN</Link></button>
-                <button type='button' className='toggle-btn'><Link to="/register">REGISTER</Link></button>
-            </div>
+    <div className="h-auto">
+      
+        <div className="w-full bg-[rgb(25,31,92)] h-[69px] flex flex-row">
+          <div className="float-left basis-1/5">
+            <img className="mt-4" src={holi1} alt="" />
+          </div>
+
+          <div className="basis-3/5">
+            <p className="mt-[7.59px] text-center text-white text-[20px] font-bold font-montserat">
+              CONTACT US
+            </p>
+            <p className="mt-[1px] text-center text-white text-[14px] font-light font-montserat">
+              GET IN TOUCH EASILY
+            </p>
+          </div>
+
+          <div className=" basis-1/5">
+            <img className="float-right" src={holi2} alt="" />
+          </div>
         </div>
 
-        <form className='reg-input-group'>
-        
-            <input type='text' className='input-field' placeholder=' Full Name' required/>
-            <input type='text' className='input-field' placeholder='Email Address' required/>
-            <input type='number' className='input-field' placeholder='Phone Number' required/>
-            <input type='password' className='input-field' placeholder='Password' required/>
-            <input type='password' className='input-field' placeholder='Confirm Password' required/>
-            <input type='number' className='input-field' placeholder='OTP Verification' required/>
+        <form >
+            <div className="w-[210px] h-[295px] bg-[rgb(25,31,92)] mt-[39px] rounded-[28px] m-auto flex flex-col">
+                <div className="w-[192px] h-[44px] bg-white  m-2 rounded-[72px] flex flex-raw">
+                        <Link to='/login'>
+                            <div className="w-[93px] h-[38px]   m-[3px] basis-1/2 hover:cursor-pointer">
+                              <p className="text-[14px] font-montserat mt-[11px] ml-5 font-black text-[rgb(25,31,92)]">LOGIN</p>
+                            </div>
+                        </Link>
+                            <div className="bg-[rgb(25,31,92)] rounded-[72px] w-[93px] h-[38px] basis-1/2 hover:cursor-pointer m-[3px]">
+                              <p className=" text-[14px] font-montserat mt-2 ml-[11px] font-black text-white">RGISTER</p>
+                            </div>
+                </div>
 
-                <div className='icon-field1'></div>
-                    <div className='name-img'>
-                        <img src={name_icon} alt="name-icon"/>
-                    </div> 
-                
-                <div className='icon-field2'></div>
-                    <div className='email-img'>
-                        <img src={email_icon} alt="email-icon"/>
-                    </div> 
+                <div className="w-[173px] h-[31px] flex flex-row ml-[18.5px] mt-2">
+                  <div className="w-[36px] h-[31px] float-left bg-white rounded-s-[5px]">
+                      <div className="mt-1 ml-[6px]"><FaCircleUser  size={24}  color="darkblue"/></div>    
+                  </div>
+                  <div className="w-[133px] h-[31px] ml-1 ">
+                      <input type="name" name="name" placeholder="Full Name " className="w-[133px] h-[31px] rounded-e-[5px] font-montserat text-[10px] font-bold"></input>
+                  </div>
+                </div>
 
-                <div className='icon-field3'></div>
-                    <div className='mobile-img'>
-                        <img src={mobile_icon} alt="mobile-icon"/>
-                    </div> 
+                <div className="w-[173px] h-[31px] flex flex-row ml-[18.5px] mt-2">
+                  <div className="w-[36px] h-[31px] float-left bg-white rounded-s-[5px]">
+                      <div className="mt-1 ml-[6px]"><FaMobile  size={24}  color="darkblue" /></div>    
+                  </div>
+                  <div className="w-[133px] h-[31px] ml-1 ">
+                      <input type="text" name="text" placeholder="Phone Number " className="w-[133px] h-[31px] rounded-e-[5px] font-montserat text-[10px] font-bold"></input>
+                  </div>
+                </div>
 
-                <div className='icon-field4'></div>
-                    <div className='password-img3'>
-                        <img src={password_icon} alt="passwoed-icon3"/>
-                    </div> 
-                <div className='icon-field5'></div>
-                    <div className='password-img2'>
-                        <img src={password_icon} alt="password-icon2"/>
+                <div className="w-[173px] h-[31px] flex flex-row ml-[18.5px] mt-2">
+                  <div className="w-[36px] h-[31px] float-left bg-white rounded-s-[5px]">
+                      <div className="mt-1 ml-[6px]"><IoMdMail size={24}  color="darkblue" /></div>    
+                  </div>
+                  <div className="w-[133px] h-[31px] ml-1 ">
+                      <input type="email" name="email" placeholder="Enter email " className="w-[133px] h-[31px] rounded-e-[5px] font-montserat text-[10px] font-bold"></input>
+                  </div>
+                </div>
+
+                <div className="w-[173px] h-[31px] flex flex-row ml-[18.5px] mt-2">
+                  <div className="w-[36px] h-[31px] float-left bg-white rounded-s-[5px]">
+                      <div className="mt-1 ml-[6px]"><RiLockPasswordFill size={24}  color="darkblue" /></div>    
+                  </div>
+                  <div className="w-[133px] h-[31px] ml-1 ">
+                      <input type="password" name="password" placeholder="Enter password " className="w-[133px] h-[31px] rounded-e-[5px] font-montserat text-[10px] font-bold"></input>
+                  </div>
+                </div>
+
+                <button>
+                    <div className=" w-[152px] h-[22px] bg-yellow-500 mt-4 m-auto rounded-full">
+                            <p className="text-white font-montserat text-[10px] font-bold text-center pt-[3px]">Verify My Email Address</p>
                     </div>
-                <div className='icon-field6'></div>
-                    <div className='password-img1'>
-                        <img src={password_icon} alt="password-icon1"/>
-                    </div> 
-                
+                </button>
+            </div>  
+
+            <div className="m-auto w-[102px] h-[36px] ">
+            <button type="submit" name="submit" className=" w-[124px] h-[36px] bg-[rgb(25,31,92)] mt-2 rounded-full flex ">
+                <img src={loginlogo} alt="" className="w-8 h-8 mt-[2px] ml-[3px]"/>
+                <p className="text-white font-montserat text-[15px] font-bold mt-2 ml-1">REGISTER</p>
+            </button>
+            </div>
         </form>
-            <div>
-                <button type='submit' className='submit-btn'> <Link to="/register">REGISTER</Link></button>
-                <div>
-                    <div id='arro'></div>  
-                    <div  className='register-icon'>
-                        <img src={register_icon} alt="register-icon" />
-                    </div>         
-                </div>          
-            </div>       
+
+        
+        
+        <div className="w-full h-[110px] bg-[rgb(25,31,92)] m-auto mt-[39px]">
+          <p className="font-bold text-white text-[14px] pt-2 text-center font-montserat">Meet Our Web Development Team</p>
+          <p className=" text-white text-[14px] text-center pt-1 font-light font-montserat">Our team consist of passionate individual dedicated to creating exeptional web experiences.</p>
+        </div>
+        
+        <div className="w-[320px] m-auto mt-5">
+          <div className="w-[320px] h-[128px]">
+            <div className="relative">
+              <div className="w-[118px] h-[118px] rounded-full bg-[rgb(25,31,92)] absolute top-[47px] left-[157px]"></div>
+              <img className="ml-20 absoloute" src={memberHoli} alt="" />
+            </div>
+          </div>
+          <div className="w-[320px] m-auto  ">
+            <ul>
+              <li className="text-[rgb(25,31,92)] font-bold text-[14px] ml-1 list-disc">Name:</li>
+                  <dd className=" text-[rgb(25,31,92)] font-light text-[14px] ml-1">W.B.W.M.R.M.C Aluwihare</dd>
+              <li className=" text-[rgb(25,31,92)] font-bold text-[14px] ml-1 list-disc">Role :</li>
+                  <dd className="text-[rgb(25,31,92)] font-light text-[14px] ml-1">Backend Developper</dd>
+              <li className="text-[rgb(25,31,92)] font-bold text-[14px] ml-1 list-disc">Expertise :</li>
+                  <dd className="text-[rgb(25,31,92)] font-light text-[14px] ml-1 list-disc">JavaScript,Css,Nodejs,React</dd>
+              <li className="text-[rgb(25,31,92)] font-bold text-[14px] ml-1 list-disc">Bio :</li>
+                  <dd className="text-[rgb(25,31,92)] font-light text-[14px] text-justify ml-1">
+                    Sample Name is a seasoned developer
+                    with a passion for crafting beautiful 
+                    and functional user interfaces.
+                  </dd>
+            </ul>
+          </div>
+        </div>
+
+        <div className="w-[320px] m-auto mt-5">
+          <div className="w-[320px] h-[164spx]">
+            <div className="relative">
+              <div className="w-[118px] h-[118px] rounded-full bg-[rgb(25,31,92)] absolute top-[47px] left-[65px]"></div>
+              <img className="absoloute" src={memberHoli2} alt="" />
+            </div>
+          </div>
+          <div className="w-[320px] m-auto  ">
+            <ul>
+              <li className="text-[rgb(25,31,92)] font-bold text-[14px] ml-1 list-disc">Name:</li>
+                  <dd className=" text-[rgb(25,31,92)] font-light text-[14px] ml-1">W.B.W.M.R.M.C Aluwihare</dd>
+              <li className=" text-[rgb(25,31,92)] font-bold text-[14px] ml-1 list-disc">Role :</li>
+                  <dd className="text-[rgb(25,31,92)] font-light text-[14px] ml-1">Backend Developper</dd>
+              <li className="text-[rgb(25,31,92)] font-bold text-[14px] ml-1 list-disc">Expertise :</li>
+                  <dd className="text-[rgb(25,31,92)] font-light text-[14px] ml-1">JavaScript,Css,Nodejs,React</dd>
+              <li className="text-[rgb(25,31,92)] font-bold text-[14px] ml-1 list-disc">Bio :</li>
+                  <dd className="text-[rgb(25,31,92)] font-light text-[14px] text-justify ml-1">
+                    Sample Name is a seasoned developer
+                    with a passion for crafting beautiful 
+                    and functional user interfaces.
+                  </dd>
+          </ul>
+          </div>
+        </div>
+
+        <div className="w-[320px] m-auto mt-5">
+          <div className="w-[320px] h-[128px]">
+            <div className="relative">
+              <div className="w-[118px] h-[118px] rounded-full bg-[rgb(25,31,92)] absolute top-[47px] left-[157px]"></div>
+              <img className="ml-20 absoloute" src={memberHoli} alt="" />
+            </div>
+          </div>
+          <div className="w-[320px] m-auto  ">
+            <ul>
+              <li className="text-[rgb(25,31,92)] font-bold text-[14px] ml-1 list-disc">Name:</li>
+                  <dd className=" text-[rgb(25,31,92)] font-light text-[14px] ml-1">W.B.W.M.R.M.C Aluwihare</dd>
+              <li className=" text-[rgb(25,31,92)] font-bold text-[14px] ml-1 list-disc">Role :</li>
+                  <dd className="text-[rgb(25,31,92)] font-light text-[14px] ml-1">Backend Developper</dd>
+              <li className="text-[rgb(25,31,92)] font-bold text-[14px] ml-1 list-disc">Expertise :</li>
+                  <dd className="text-[rgb(25,31,92)] font-light text-[14px] ml-1 list-disc">JavaScript,Css,Nodejs,React</dd>
+              <li className="text-[rgb(25,31,92)] font-bold text-[14px] ml-1 list-disc">Bio :</li>
+                  <dd className="text-[rgb(25,31,92)] font-light text-[14px] text-justify ml-1">
+                    Sample Name is a seasoned developer
+                    with a passion for crafting beautiful 
+                    and functional user interfaces.
+                  </dd>
+            </ul>
+          </div>
+        </div>
+
+        <div className="w-[320px] m-auto mt-5">
+          <div className="w-[320px] h-[164spx]">
+            <div className="relative">
+              <div className="w-[118px] h-[118px] rounded-full bg-[rgb(25,31,92)] absolute top-[47px] left-[65px]"></div>
+              <img className="absoloute" src={memberHoli2} alt="" />
+            </div>
+          </div>
+          <div className="w-[320px] m-auto  ">
+            <ul>
+              <li className="text-[rgb(25,31,92)] font-bold text-[14px] ml-1 list-disc">Name:</li>
+                  <dd className=" text-[rgb(25,31,92)] font-light text-[14px] ml-1">W.B.W.M.R.M.C Aluwihare</dd>
+              <li className=" text-[rgb(25,31,92)] font-bold text-[14px] ml-1 list-disc">Role :</li>
+                  <dd className="text-[rgb(25,31,92)] font-light text-[14px] ml-1">Backend Developper</dd>
+              <li className="text-[rgb(25,31,92)] font-bold text-[14px] ml-1 list-disc">Expertise :</li>
+                  <dd className="text-[rgb(25,31,92)] font-light text-[14px] ml-1">JavaScript,Css,Nodejs,React</dd>
+              <li className="text-[rgb(25,31,92)] font-bold text-[14px] ml-1 list-disc">Bio :</li>
+                  <dd className="text-[rgb(25,31,92)] font-light text-[14px] text-justify ml-1">
+                    Sample Name is a seasoned developer
+                    with a passion for crafting beautiful 
+                    and functional user interfaces.
+                  </dd>
+          </ul>
+          </div>
+        </div>
+
+        <div className="m-auto w-[320px]">
+          <img src={jenzeict2} alt=""className="m-auto"/>
+        </div>
+        
+      
     </div>
-  )
+  );
 }
 
-export default Register
+export default Login;
