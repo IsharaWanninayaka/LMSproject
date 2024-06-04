14ff7f3225b43d28e218cc640ff4a23f3be24d08
devindawanasinghe <devindawanasinghe@gmail.com>
2024-06-04 23:31:40 +0530

    Admin penal user details geting

--- src/components/admin/admin-panels/BasicDetails.jsx
+++ src/components/admin/admin-panels/BasicDetails.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
-import HeaderBar from '../admin-components/navigateBar/HeaderBar'
-import "../admin-style/basicdetails.scss"
+import React, { useEffect, useState } from 'react';
+import HeaderBar from '../admin-components/navigateBar/HeaderBar';
+import "../admin-style/basicdetails.scss";
+import axios from 'axios';
+//import React from 'react';
 
-function BasicDetails() {
+//data fetch
+const List = () => {
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  const fetchData = async () => {
+    try {
+      const result = await axios("http://localhost:3001/users");
+      //console.log(result.data);
+      setUserData(result.data)
+    } catch (err) {
+      console.log("Something wrong");
+    }
+  }
   return (
     <div>
         <HeaderBar/>
@@ -12,48 +30,30 @@ function BasicDetails() {
           <h2>User Details</h2>
           <ul class="res-table">
             <li class="table-header">
-              <div class="col col-1">User Id</div>
-              <div class="col col-2">Name</div>
-              <div class="col col-3">Email</div>
-              <div class="col col-4">Mobile Number</div>
+              <div class="col tcol-1">User Id</div>
+              <div class="col tcol-2">Name</div>
+              <div class="col tcol-3">Email</div>
+              <div class="col tcol-4">Mobile Number</div>
             </li>
-            <li class="table-row">
-              <div class="col col-1" data-label="User Id">0001</div>
-              <div class="col col-2" data-label="User Name">chalith</div>
-              <div class="col col-3" data-label="User Email">chalith@gmail.com</div>
-              <div class="col col-4" data-label="Mobile Number">077 8663845</div>
-            </li>
-            <li class="table-row">
-              <div class="col col-1" data-label="Job User">   0002  </div>
-              <div class="col col-2" data-label="User Name">     </div>
-              <div class="col col-3" data-label="User Email">    </div>
-              <div class="col col-4" data-label="Mobile Number">    </div>
-            </li>
-            <li class="table-row">
-              <div class="col col-1" data-label="User Id">    0003 </div>
-              <div class="col col-2" data-label="User Name">    </div>
-              <div class="col col-3" data-label="User Email">    </div>
-              <div class="col col-4" data-label="Mobile Number">   </div>
-            </li>
-            <li class="table-row">
-              <div class="col col-1" data-label="User Id">     </div>
-              <div class="col col-2" data-label="User Name">     </div>
-              <div class="col col-3" data-label="User Email">      </div>
-              <div class="col col-4" data-label="Mobile Number">      </div>
-            </li>
-            <li class="table-row">
-              <div class="col col-1" data-label="User Id">     </div>
-              <div class="col col-2" data-label="User Name">     </div>
-              <div class="col col-3" data-label="User Email">      </div>
-              <div class="col col-4" data-label="Mobile Number">      </div>
-            </li>
+            <tbody class="table-row">
+              {
+                  userData.map((user, i) => {
+                     return (
+                        <tr key={i}>
+                        <td class="col col-1" >{i+1}</td>
+                        <td class="col col-2" >{user.name}</td>
+                        <td class="col col-3" >{user.email}</td>
+                        <td class="col col-4" >{user.phone}</td>
+                     </tr>
+                   )
+                  })
+              }            
+            </tbody>
           </ul>
         </div>
       </div>
     </div>
-
-
   )
 }
 
-export default BasicDetails
+export default List;
